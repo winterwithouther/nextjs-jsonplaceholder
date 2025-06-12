81a5140a10aebc82874f95c4f33f9d6dfb067fd2
Eman <egurung1117@gmail.com>
2025-06-11 21:15:47 -0400

    loading for comments

--- src/app/posts/[id]/loading.tsx
+++ src/app/posts/[id]/loading.tsx
@@ -8,6 +8,13 @@ export default function Loading() {
                 <div className="h-4 bg-gray-200 rounded w-5/6"></div>
                 <div className="h-4 bg-gray-200 rounded w-4/6"></div>
             </div>
+
+            <h3 className="text-lg font-semibold mt-8 mb-2">Loading comments...</h3>
+            <ul className="space-y-4">
+                <li className="bg-gray-100 p-4 rounded animate-pulse h-20"/>
+                <li className="bg-gray-100 p-4 rounded animate-pulse h-20"/>
+                <li className="bg-gray-100 p-4 rounded animate-pulse h-20" />
+            </ul>
         </main>
     </>)
 }
